f73e21632f0381d240297713335f5d7ee33430fa
João Souza <marceloamaral201924@gmail.com>
2025-03-15 19:03:28 -0400

    fix: now the database has only one table

--- sqls/coffee_sales_db.sql
+++ sqls/coffee_sales_db.sql
@@ -2,47 +2,22 @@ SELECT table_name
 FROM information_schema.tables
 WHERE table_schema = 'public';
 
-CREATE TABLE customers 
+CREATE TABLE coffee_beans_sales 
 ( 
-    customer_id SERIAL PRIMARY KEY,  
-    city VARCHAR(20) NOT NULL
-);
-
-CREATE TABLE products 
-( 
-    product_id SERIAL PRIMARY KEY,
-    category VARCHAR(25) NOT NULL,  
-    product_name VARCHAR(25) NOT NULL,    
-    unit_price NUMERIC(10,2) NOT NULL  
-); 
-
-CREATE TABLE sales 
-( 
-    sale_id SERIAL PRIMARY KEY,  
-    date DATE NOT NULL,  
-    quantity INT NOT NULL,  
-    sales_amount NUMERIC(10,2) NOT NULL,  
+    id SERIAL PRIMARY KEY,  
+	date DATE NOT NULL,
+	customer_id INT NOT NULL,
+    city VARCHAR(20) NOT NULL,
+	category VARCHAR(25) NOT NULL,  
+    product VARCHAR(25) NOT NULL,    
+    unit_price NUMERIC(10,2) NOT NULL,
+	quantity INT NOT NULL,
+	sales_amount NUMERIC(10,2) NOT NULL,  
     used_discount BOOLEAN NOT NULL,  
     discount_amount NUMERIC(10,2) NOT NULL,  
-    final_sales NUMERIC(10,2) NOT NULL,  
-    customer_id INT NOT NULL,  
-    product_id INT NOT NULL  
+    final_sales NUMERIC(10,2) NOT NULL
 );
 
-ALTER TABLE sales ADD FOREIGN KEY(customer_id) REFERENCES customers (customer_id);
-ALTER TABLE sales ADD FOREIGN KEY(product_id) REFERENCES products (product_id);
+DROP TABLE coffee_beans_sales;
 
-
-DROP TABLE customers;
-
-DROP TABLE products;
-
-DROP TABLE sales;
-
-SELECT * FROM customers;
-
-SELECT * FROM products;
-
--- ON CONFLICT (customer_id) 
---             DO UPDATE SET
---                 city = EXCLUDED.city
+SELECT * FROM coffee_beans_sales;
